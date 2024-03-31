dcf988e6995dc94d7a5e620eabd652373597b872
Karl Eklund <localpart@gmail.com>
2024-04-01 00:34:05 +0200

    Command to delete blank lines

--- lib/titles.ex
+++ lib/titles.ex
@@ -2,11 +2,29 @@ defmodule Titles do
   require Logger
 
   def start(:normal, []) do
-    [_, _, "--", file] = Burrito.Util.Args.get_arguments() |> IO.inspect()
+    case Burrito.Util.Args.get_arguments() do
+      [_mix, "run", "--", command, file] ->
+        result =
+          case File.read(file) do
+            {:ok, input} ->
+              case command do
+                "url-fix" -> {:ok, Titles.convert(input)}
+                "compact" -> {:ok, Titles.compact(input)}
+                other -> {:error, "Unknown command: #{other}"}
+              end
 
-    case File.read(file) do
-      {:ok, input} -> Titles.convert(input)
-      _ -> IO.puts("No file given")
+            _ ->
+              {:error, "Failed to open file #{file}"}
+          end
+
+        case result do
+          {:ok, output} -> File.write!(file, output)
+          {:error, message} -> IO.puts("Error: #{message}")
+        end
+
+      [arg1, arg2 | _] ->
+        IO.puts("Usage: #{arg1} #{arg2} -- <command> <input file>")
+        IO.puts("Where <command> is one of: url-fix, compact")
     end
 
     # System.halt(0)
@@ -16,21 +34,29 @@ defmodule Titles do
     {:ok, self()}
   end
 
+  def compact(input) do
+    input
+    |> String.split("\n")
+    |> Enum.reject(fn line -> line == "" end)
+    |> Enum.join("\n")
+    |> Kernel.<>("\n")
+  end
+
   def convert(input) do
     input
     |> String.split()
-    |> Enum.map(fn url ->
+    |> Enum.map(fn line ->
       IO.write("url -> ")
 
       {:ok, document} =
-        Tesla.get!(url).body
+        Tesla.get!(line).body
         |> Floki.parse_document()
 
       [{"title", _, [title]}] = Floki.find(document, "title")
 
       IO.write(title <> "\n")
 
-      "- [#{title}](#{url})\n"
+      "- [#{title}](#{line})\n"
     end)
     |> Enum.join()
   end
--- test/titles_test.exs
+++ test/titles_test.exs
@@ -2,20 +2,39 @@ defmodule TitlesTest do
   use ExUnit.Case
   doctest Titles
 
-  test "Converts list of URLs to a Markdown document with links" do
-    input = """
-    https://www.google.com/
-    https://www.svt.se/
-    """
+  describe "convert" do
+    test "Converts list of URLs to a Markdown document with links" do
+      input = """
+      https://www.google.com/
+      https://www.svt.se/
+      """
 
-    expected = """
-    - [Google](https://www.google.com/)
-    - [SVT Nyheter](https://www.svt.se/)
-    """
+      expected = """
+      - [Google](https://www.google.com/)
+      - [SVT Nyheter](https://www.svt.se/)
+      """
 
-    assert Titles.convert(input) == expected
+      assert Titles.convert(input) == expected
+    end
+
+    @tag :skip
+    test "Ignore stuff that's not URLs"
   end
 
-  @tag :skip
-  test "Ignore stuff that's not URLs"
+  describe "compact" do
+    test "removes empty lines" do
+      input = """
+      Hello
+
+      World
+      """
+
+      expected = """
+      Hello
+      World
+      """
+
+      assert Titles.compact(input) == expected
+    end
+  end
 end
